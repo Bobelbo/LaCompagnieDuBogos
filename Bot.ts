66c64af6962b3ccc9a71e3255e3f344f5d2f32f6
Julien Brisson <1980037@cstjean.qc.ca>
2023-01-14 12:44:36 -0500

    Money + l + ration + 3

--- Bot.ts
+++ Bot.ts
@@ -7,6 +7,8 @@ import { TowerType } from './types/Tower';
 export class Bot {
     private state: GameTick;
     private positionArr: IValuedPositions[];
+    private round: number;
+    private money: number;
 
     constructor() {
         console.log('Initializing your super duper mega bot');
@@ -15,39 +17,46 @@ export class Bot {
     // Main
     getActions(gameMessage: GameTick): Command[] {
         this.state = gameMessage;
-        if (gameMessage.round === 0) { this.gameSetup() }
+        if (gameMessage.round === 0) {
+            this.gameSetup();
+            this.round = -1;
+        }
 
         return this.coreLoop();
     }
 
     gameSetup() {
         this.positionArr = new MapUtils(this.state.map).getPositionArray();
-        console.log(this.printArr(this.positionArr))
-    }
-
-    printArr(arr) {
-        let str = "";
-        for (let item of arr) {
-            if (Array.isArray(item)) str += this.printArr(item);
-            else str += item + ", ";
-        }
-        return str;
     }
 
     coreLoop(): Command[] {
         let commands = [];
+        this.money = this.state.teamInfos[this.state.teamId].money;
 
-        commands.push(this.getTowerCommand());
-        new ReinforcementUtils(this.state).getReinforcementAction().forEach((x) => commands.push(x))
+        this.getRoundActions().forEach((x) => commands.push(x))
+        this.getReinforcementCommand().forEach((x) => commands.push(x))
+
+        this.round = this.state.round;
 
         console.log(commands)
         return commands;
     }
 
+    getRoundActions() {
+        if (this.round >= this.state.round) return [];
+
+        return this.getTowerCommand();
+    }
+
+    getReinforcementCommand() {
+        let cmd = new ReinforcementUtils(this.state).getReinforcementAction(this.money);
+        return cmd;
+    }
+
     getTowerCommand() {
-        let pos = this.positionArr.pop()
-        console.log(pos)
-        return new BuildCommand(TowerType.SPEAR_SHOOTER, pos.position)
+        let cmd = [new BuildCommand(TowerType.SPEAR_SHOOTER, this.positionArr.pop().position)];
+        this.money -= 200;
+        return cmd;
     }
 }
 
--- lib/reinforcement.ts
+++ lib/reinforcement.ts
@@ -6,10 +6,12 @@ import { EnemyType } from "../types/Enemy";
 class ReinforcementRoi {
     public type: EnemyType
     public roi: number
+    public price: number;
 
-    constructor(type: EnemyType, roi: number) {
+    constructor(type: EnemyType, roi: number, price: number) {
         this.type = type;
         this.roi = roi;
+        this.price = price;
     }
 }
 
@@ -22,29 +24,45 @@ export class ReinforcementUtils {
         this.state = state;
     }
 
-    getReinforcementAction(): CommandAction[] {
+    getReinforcementAction(money: number): CommandAction[] {
+        let enemies: ReinforcementRoi[] = this.getBestReinforcement()
         const gameHelper = new GameHelpers(this.state);
         const { enemyTeams } = gameHelper;
         this.actions = [];
 
-        if (this.state.teamInfos[this.state.teamId].money > 15)
-            this.actions.push(new SendReinforcementCommand(EnemyType.LVL2, enemyTeams[0]));
+        enemies.reverse().forEach((e: ReinforcementRoi) => {
+            while (money-e.price >= 0) {
+                this.actions.push(new SendReinforcementCommand(e.type, this.state.teamInfos[0].id));
+            }
+        })
 
         return this.actions;
     }
-    /*
-        private getBestReinforcement(): EnemyType[] {
-            let roiArr: ReinforcementRoi[] = [];
-    
-            Object.keys(this.state.shop.reinforcements).forEach(
-                (type: string) => {
-                    roiArr.push(new ReinforcementRoi(
-                        type as EnemyType,
-                        this.state.shop.reinforcements[type].payoutBonus /
-                        this.state.shop.reinforcements[type].price))
-                }
-            )
-    
-            return roiArr.sort((a, b) => a.roi - b.roi).map((x) => x.type);
-        }*/
+
+    private getBestReinforcement(): ReinforcementRoi[] {
+        let roiArr: ReinforcementRoi[] = [];
+
+        Object.keys(this.state.shop.reinforcements).forEach(
+            (type: string) => {
+                roiArr.push(new ReinforcementRoi(
+                    type as EnemyType,
+                    this.state.shop.reinforcements[type].payoutBonus /
+                    this.state.shop.reinforcements[type].price,
+                    this.state.shop.reinforcements[type].price,
+                    ))
+            }
+        )
+
+        return roiArr.sort((a, b) => a.roi - b.roi);
+    }
 }
+
+// let enemies: EnemyType[] = this.getBestReinforcement()
+//         const gameHelper = new GameHelpers(this.state);
+//         const { enemyTeams } = gameHelper;
+//         this.actions = [];
+
+//         if (this.state.teamInfos[this.state.teamId].money > enemies[0].)
+            
+
+//         return this.actions;
